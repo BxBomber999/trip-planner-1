2840aa47cc1de08d33ee41e5ad04ea689c702475
stloder <scl963@gmail.com>
2018-03-20 11:58:55 -0400

    Added map using webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 console.log('running index.js');
 console.log("hello, world")
+
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1Ijoic3Rsb2RlciIsImEiOiJjamV6dGtjNmUwNmh4MnBrOWliaTczaGZrIn0.Yz50-14PvZAw5ezq_yOp2g";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const markerEl = document.createElement("div");
+markerEl.style.width = "32px";
+markerEl.style.height = "39px";
+markerEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)'
+
+new mapboxgl.Marker(markerEl).setLngLat([-74.009151, 40.705086]).addTo(map)
---
+++ src/marker.js
@@ -0,0 +1,23 @@
+function createMarkerElement (type, coordsArr) {
+   
+
+
+const markerEl = document.createElement("div");
+markerEl.style.width = "32px";
+markerEl.style.height = "39px";
+
+if(type === 'activity') {
+    markerEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)'  
+} else if (type === 'hotel') {
+markerEl.style.backgroundImage = 'url(http://i.imgur.com/D9574Cu.png)'
+} else {
+    markerEl.style.backgroundImage = 'url(http://i.imgur.com/cqR6pUI.png)'
+}
+
+new mapboxgl.Marker(markerEl).setLngLat([-74.009151, 40.705086]).addTo(map)
+
+}
+
+module.exports = {
+    createMarkerElement
+}
